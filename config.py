6db56443cf3bc0f1c6f3e6167c14004a36771670
Moni Sandy <148899466+MoniSandy0204@users.noreply.github.com>
2024-01-15 18:53:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mukeshmoni/Sweety-Music",
+    "https://github.com/MoniSandy0204/SweetyMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
